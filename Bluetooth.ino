8c9abe027018731f21cb4039be36a614a7896b28
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2016-05-16 19:23:43 +0200

    Update Bluetooth.ino

--- Bluetooth.ino
+++ Bluetooth.ino
@@ -1,4 +1,5 @@
 
+
 /*
   VEHÍCULO CONTROLADO POR BLUETOOTH Y APP
   Programa para control de printbot kiwibot por APP y HC06.
@@ -9,25 +10,31 @@
   APP: https://play.google.com/store/apps/details?id=appinventor.ai_albyuda.HC06
 */
 
-// LEDS
-const int ledPinR = 6;
-const int ledPinG = 5;
-const int ledPinB = 9;
-//Timbre
-const int buzzerPin = 10;
+// variables constantes para los pines
 // Pines de los servos
 const int mLeft = 7; // servo izquierda
 const int mRight = 4;  // servo derecha
+// pines LED RGB
+const int ledPinR = 6;
+const int ledPinG = 5;
+const int ledPinB = 9;
+// pin timbre
+const int buzzerPin = 10;
+
 // variable para almacenar lectura puerto serial
 int incomingByte;
 // variable para almacenar el modo de movimiento
 int mode = 's'; // lo inicializamos en parado
 
-
 void setup() {
+  // abrimos el puerto serie para la comunicacion
   Serial.begin(9600);
+  // declaramos como salida servo, RGB, zumbador
   pinMode(mLeft, OUTPUT);
   pinMode(mRight, OUTPUT);
+  pinMode(ledPinR, OUTPUT);
+  pinMode(ledPinG, OUTPUT);
+  pinMode(ledPinB, OUTPUT);
 }
 
 void loop() {
@@ -36,6 +43,7 @@ void loop() {
     incomingByte = Serial.read(); // Fijamos la variable incomingByte a lo que lea en el serial
 
     //guardamos en variable mode la direccion de funcionamiento del vehiculo
+    // segun recibamos s, f, b, r, l
     if (incomingByte == 's') {
       mode = 's';
     }
@@ -52,29 +60,30 @@ void loop() {
       mode = 'l';
     }
 
-    //Aqui encendemos totalmente los LEDs
+    //si recibimos H encendemos los LEDs
     if (incomingByte == 'H') {
       digitalWrite(ledPinR, HIGH);
       digitalWrite(ledPinG, HIGH);
       digitalWrite(ledPinB, HIGH);
     }
-    //Aqui apagamos los LEDs
+    //si recibimos L apagamos los LEDs
     if (incomingByte == 'L') {
       digitalWrite(ledPinR, LOW);
       digitalWrite(ledPinG, LOW);
       digitalWrite(ledPinB, LOW);
     }
-    //En los proximos 2 bloques encendemos o apagamos el timbre
+    //si recibimos Z activamos timbre
     if (incomingByte == 'Z') {
-      digitalWrite(buzzerPin, HIGH);//Estos para el timbre
+      digitalWrite(buzzerPin, HIGH);
     }
+    //si recibimos z apagamos timbre
     if (incomingByte == 'z') {
       digitalWrite(buzzerPin, LOW);
     }
+    // pequeño delay para recibir datos
     delay(15);
   }
-
-
+  // activamos el modo de funcionamiento
   if (mode == 's') {
     stopA();
   }
@@ -91,9 +100,9 @@ void loop() {
     left();
   }
 
-
-
 }
+
+// funciones del motor
 
 void frontward () {
 
