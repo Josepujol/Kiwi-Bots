8a25a7a104f4faf02e7bd80b095685f51c36932d
Jose Pujol Perez <Josepujol@users.noreply.github.com>
2016-05-16 19:15:09 +0200

    Update Bluetooth.ino

--- Bluetooth.ino
+++ Bluetooth.ino
@@ -1,9 +1,13 @@
+
 /*
-VEHÍCULO CONTROLADO POR BLUETOOTH Y APP
- Programa para control de printbot kiwibot por APP y HC06.
- Desarrollado por Miguel Granero, Jose Pujol y Alberto Pumar.
- Revisado Noviembre 2015
- */
+  VEHÍCULO CONTROLADO POR BLUETOOTH Y APP
+  Programa para control de printbot kiwibot por APP y HC06.
+  al recibir s para; f adelante; b atras; r derecha; l izquierda;
+  H enciende LEDs, L apaga LEDs; Z zumbador; z apaga el zumbador
+  Desarrollado por Miguel Granero, Jose Pujol y Alberto Pumar.
+  Revisado Mayo 2016
+*/
+
 // LEDS
 const int ledPinR = 6;
 const int ledPinG = 5;
@@ -11,96 +15,122 @@ const int ledPinB = 9;
 //Timbre
 const int buzzerPin = 10;
 // Pines de los servos
-const int MLeft = 7;
-const int MRight = 4;
+const int mLeft = 7; // servo izquierda
+const int mRight = 4;  // servo derecha
 // variable para almacenar lectura puerto serial
 int incomingByte;
+// variable para almacenar el modo de movimiento
+int mode = 's'; // lo inicializamos en parado
+
 
 void setup() {
   Serial.begin(9600);
-  pinMode(MLeft, OUTPUT);
-  pinMode(MRight, OUTPUT);
+  pinMode(mLeft, OUTPUT);
+  pinMode(mRight, OUTPUT);
 }
 
 void loop() {
+  // si hay datos en el puerto serie
   if (Serial.available() > 0) {
     incomingByte = Serial.read(); // Fijamos la variable incomingByte a lo que lea en el serial
+
+    //guardamos en variable mode la direccion de funcionamiento del vehiculo
+    if (incomingByte == 's') {
+      mode = 's';
+    }
+    if (incomingByte == 'f') {
+      mode = 'f';
+    }
+    if (incomingByte == 'b') {
+      mode = 'b';
+    }
+    if (incomingByte == 'r') {
+      mode = 'r';
+    }
+    if (incomingByte == 'l') {
+      mode = 'l';
+    }
+
+    //Aqui encendemos totalmente los LEDs
+    if (incomingByte == 'H') {
+      digitalWrite(ledPinR, HIGH);
+      digitalWrite(ledPinG, HIGH);
+      digitalWrite(ledPinB, HIGH);
+    }
+    //Aqui apagamos los LEDs
+    if (incomingByte == 'L') {
+      digitalWrite(ledPinR, LOW);
+      digitalWrite(ledPinG, LOW);
+      digitalWrite(ledPinB, LOW);
+    }
+    //En los proximos 2 bloques encendemos o apagamos el timbre
+    if (incomingByte == 'Z') {
+      digitalWrite(buzzerPin, HIGH);//Estos para el timbre
+    }
+    if (incomingByte == 'z') {
+      digitalWrite(buzzerPin, LOW);
+    }
+    delay(15);
   }
-  //Asociamos la memoria a estos codigos que indican la direccion del motor
- 
-  //Aqui hacemos con los motores lo que hayamos recibido por el serial, como hemos fijado arriba
-  if (incomingByte == 's') {
+
+
+  if (mode == 's') {
     stopA();
   }
-  if (incomingByte == 'f') {
-    frontwards();
+  if (mode == 'f') {
+    frontward();
   }
-  if (incomingByte == 'l') {
-    left();
-  }
-  if (incomingByte == 'b') {
+  if (mode == 'b') {
     backward();
   }
-  if (incomingByte == 'r') {
+  if (mode == 'r') {
     right();
   }
-  //Aqui encendemos totalmente los LEDs
-  if (incomingByte == 'H') {
-    analogWrite(ledPinR, 255);
-    analogWrite(ledPinG, 255);
-    analogWrite(ledPinB, 255);
+  if (mode == 'l') {
+    left();
   }
-  //Aqui apagamos los LEDs
-  if (incomingByte == 'L') {
-    analogWrite(ledPinR, 0);
-    analogWrite(ledPinG, 0);
-    analogWrite(ledPinB, 0);
-  }
-  //En los proximos 2 bloques encendemos o apagamos el timbre
-  if (incomingByte == 'Z') {
-    digitalWrite(buzzerPin, HIGH);//Estos para el timbre
-  }
-  if (incomingByte == 'z') {
-    digitalWrite(buzzerPin, LOW);
-  }
-  delay(15);
+
+
+
 }
 
-void frontwards () {
-  digitalWrite(MLeft, HIGH);
-  digitalWrite(MRight, HIGH);
+void frontward () {
+
+  digitalWrite(mLeft, HIGH);
+  digitalWrite(mRight, HIGH);
   delayMicroseconds(1000);
-  digitalWrite(MLeft, LOW);
+  digitalWrite(mLeft, LOW);
   delayMicroseconds(1000);
-  digitalWrite(MRight, LOW);
+  digitalWrite(mRight, LOW);
   delayMicroseconds(18000);
 }
 void backward() {
-  digitalWrite(MRight, HIGH);
-  digitalWrite(MLeft, HIGH);
+  digitalWrite(mRight, HIGH);
+  digitalWrite(mLeft, HIGH);
   delayMicroseconds(1000);
-  digitalWrite(MRight, LOW);
+  digitalWrite(mRight, LOW);
   delayMicroseconds(1000);
-  digitalWrite(MLeft, LOW);
+  digitalWrite(mLeft, LOW);
   delayMicroseconds(18000);
 }
-void left(){
-  digitalWrite(MLeft, HIGH);
-  digitalWrite(MRight, HIGH);
+
+void left() {
+  digitalWrite(mLeft, HIGH);
+  digitalWrite(mRight, HIGH);
   delayMicroseconds(2000);
-  digitalWrite(MLeft, LOW);
-  digitalWrite(MRight, LOW);
+  digitalWrite(mLeft, LOW);
+  digitalWrite(mRight, LOW);
   delayMicroseconds(18000);
 }
-void right(){
-  digitalWrite(MLeft, HIGH);
-  digitalWrite(MRight, HIGH);
+void right() {
+  digitalWrite(mLeft, HIGH);
+  digitalWrite(mRight, HIGH);
   delayMicroseconds(1000);
-  digitalWrite(MLeft, LOW);
-  digitalWrite(MRight, LOW);
+  digitalWrite(mLeft, LOW);
+  digitalWrite(mRight, LOW);
   delayMicroseconds(19000);
 }
 void stopA() {
-  digitalWrite(MLeft, LOW);
-  digitalWrite(MRight, LOW);
+  digitalWrite(mLeft, LOW);
+  digitalWrite(mRight, LOW);
 }
